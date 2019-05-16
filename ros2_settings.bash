7a0b1fdd28b0544201382040cd42c87e29b03a08
Kensuke Saito <33274231+bonohub13@users.noreply.github.com>
2019-05-16 12:08:44 +0900

    Update ros2_settings.bash

--- ros2_settings.bash
+++ ros2_settings.bash
@@ -2,17 +2,17 @@ sudo apt install python3-colcon-common-extensions -y
 
 ######### ros2 on Linux: 101 #########
 
-# to fully automate the process, just remove the "#" below to deactivate comment
-# mkdir -p ~/ros2_ws/src # change workspace name, if needed(MAKE SURE TO CHANGE ALL DIR NAMES BELOW!)
-# cd ~/ros2_ws/src
+# to fully automate the process, just remove the "#" below to deactivate comment <- please, ignore
+mkdir -p ~/ros2_ws/src # change workspace name, if needed(MAKE SURE TO CHANGE ALL DIR NAMES BELOW!)
+cd ~/ros2_ws/src
 
 # === git installation ===
-# git clone https://github.com/ros2/examples.git
+git clone https://github.com/ros2/examples.git
 
-# cd examples
-# git checkout crystal # if your version differs, change from "crystal"
-# cd ~/ros2_ws
+cd examples
+git checkout crystal # if your version differs, change from "crystal"
+cd ~/ros2_ws
 # --------------------------------------------------------------------------------------------------
-# colcon build --symlink-install
-# colcon test
-# . install/setup.bash
+colcon build --symlink-install
+colcon test
+. install/setup.bash
