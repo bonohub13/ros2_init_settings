5a25b0921b2a26ccffd4e937135e3d4f95107999
Kensuke Saito <33274231+bonohub13@users.noreply.github.com>
2019-05-16 11:59:37 +0900

    Create lazy_way.bash

---
+++ lazy_way.bash
@@ -0,0 +1,7 @@
+mkdir -p ~/gitfiles/ros2_init_settings
+git clone https://github.com/bonohub13/ros2_init_settings.git ~/gitfiles/ros2_init_settings
+cd ~/gitfiles/ros2_init_settings
+
+source ros2_init.bash
+source ros2_env_init_settings.bash
+source ros2_settings.bash
