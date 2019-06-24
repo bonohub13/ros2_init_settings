756f312c0c4a30b8279f830edacc79e5e6123cb3
Kensuke Saito <ken.saito.0813@gmail.com>
2019-06-24 13:34:33 +0900

    Update ros2_env_init_settings.bash

--- ros2_env_init_settings.bash
+++ ros2_env_init_settings.bash
@@ -4,7 +4,6 @@ sudo apt install python3-argcomplete -y
 sudo apt install python3-pip -y
 sudo pip3 install argcomplete
 source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash
-echo "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" >> ~/.bashrc
 sudo apt update
 sudo apt install ros-$CHOOSE_ROS_DISTRO-rmw-opensplice-cpp -y # for OpenSplice
 sudo apt install ros-$CHOOSE_ROS_DISTRO-rmw-connext-cpp -y # for RTI Connext (requires license agreement)
