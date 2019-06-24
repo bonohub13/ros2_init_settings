5978a38f9aa50c2262e4d613d68e41d0a08c82fc
Kensuke Saito <ken.saito.0813@gmail.com>
2019-06-24 13:44:02 +0900

    Update ros2_env_init_settings.bash

--- ros2_env_init_settings.bash
+++ ros2_env_init_settings.bash
@@ -9,3 +9,13 @@ sudo apt install ros-$CHOOSE_ROS_DISTRO-rmw-opensplice-cpp -y # for OpenSplice
 sudo apt install ros-$CHOOSE_ROS_DISTRO-rmw-connext-cpp -y # for RTI Connext (requires license agreement)
 sudo apt update
 sudo apt install ros-$CHOOSE_ROS_DISTRO-turtlebot2-* -y
+
+# add ROS_SETTINGS to your .bashrc
+echo 'Do you want to switch over ros and ros2? [y/n]'
+read choice
+if [ $choice = 'y' ] || [ $choice = 'Y' ]; then
+  echo "##### ROS | ROS2 settings ##########" >> .bashrc
+  echo "alias mode=\". ~/ros_settings.sh\"" >> .bashrc
+  echo "Place the \"ros_settings.sh\" file to home directory."
+  echo "####################################" >> .bashrc
+fi
